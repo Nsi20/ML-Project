e9fb1df1de16f844213d06b27db2ccbc44ed2bdd
Nsi20 <nsidibedaniel622@gmail.com>
2025-05-26 12:29:52 +0100

    data ingestion

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ from typing import List
 
 HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->list[str]:
-  """
-  This function will return the list of requirements
-  """
-  requirements=[]
-  with open(file_path) as file_obj:
-    requirements=file_obj.readlines()
-    requirements=[req.replace("\n","") for req in requirements]
+    """
+    This function will return the list of requirements
+    """
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
-    if HYPEN_E_DOT in requirements:
-      requirements.remove(HYPEN_E_DOT)      
-
-      return requirements
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)      
+    
+    return requirements  # Fix: Unindent this line
 
 setup(
     name='mlproject',
